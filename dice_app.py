dbdd3c50f85a7ce2f19102e5d1113cdd5e4096be
shahad-b719 <shahadramzi@gmail.com>
2025-09-30 15:20:31 -0400

    dice_app.py

--- dice_app.py
+++ dice_app.py
@@ -9,10 +9,9 @@ import random
 import pandas as pd
 from PIL import Image
 
-# Title
-###st.title("🎲 Virtual Dice Roller")
+st.title("🎲 Virtual Dice Roller with Images")
 
-# Initialize session state to store rolls
+# Initialize session state
 if "rolls" not in st.session_state:
     st.session_state.rolls = []
 
@@ -22,10 +21,26 @@ if st.button("Roll the Dice!"):
     st.session_state.rolls.append(roll)
     st.success(f"You rolled a {roll}")
 
-# Show all rolls as a histogram
+    # Show big image for current roll
+   # Show big image for current roll
+    img = Image.open(fr"C:\Users\shahad\Desktop\dice app\dice{roll}.png")
+    st.image(img, width=150)
+
+# Show previous rolls as small dice images
 if st.session_state.rolls:
+    st.write("Previous rolls:")
+    cols = st.columns(len(st.session_state.rolls))
+  # Show previous rolls as small dice images
+    for i, r in enumerate(st.session_state.rolls):
+      img = Image.open(fr"C:\Users\shahad\Desktop\dice app\dice{r}.png")
+      cols[i].image(img, width=50)
+
+    # Show histogram of all rolls
     df = pd.DataFrame({"Roll": st.session_state.rolls})
     st.bar_chart(df["Roll"].value_counts().sort_index())
+
+
+
 
 
 
